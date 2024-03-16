56b266592d5330c023fdd93d82309cc01bfa3e56
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-16 17:02:39 -0500

    task 8: how on earth is this worth 5 whole points but task 6 was worth only 1 more point than that?????

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -2,16 +2,17 @@ const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLList, Gr
 const lodash = require('lodash');
 const mongoose = require('mongoose');
 const Project = require('../models/project');
+const Task = require('../models/task');
 
-const tasks = [
-  { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)', projectId: '1' },
-  { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order', projectId: '1' }
-];
+// const tasks = [
+//   { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)', projectId: '1' },
+//   { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order', projectId: '1' }
+// ];
 
-const projects = [
-  { id: '1', title: 'Advanced HTML', weight: 1, description: `Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” it's normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!` },
-  { id: '2', title: 'Bootstrap', weight: 1, description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.' }
-];
+// const projects = [
+//   { id: '1', title: 'Advanced HTML', weight: 1, description: `Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don't worry, the final page will be “ugly” it's normal, it's not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!` },
+//   { id: '2', title: 'Bootstrap', weight: 1, description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.' }
+// ];
 
 const TaskType = new GraphQLObjectType({
   name: 'Type',
@@ -23,7 +24,7 @@ const TaskType = new GraphQLObjectType({
     project: {
       type: ProjectType,
       resolve(parent, args) {
-        return lodash.find(projects, {id: parent.projectId});
+        return lodash.find(Project, {id: parent.projectId});
       }
     }
   })
@@ -39,7 +40,7 @@ const ProjectType = new GraphQLObjectType({
     tasks: {
       type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return lodash.filter(tasks, {projectId: parent.id});
+        return lodash.filter(Task, {projectId: parent.id});
       }
     }
   })
@@ -74,26 +75,26 @@ const RootQuery = new GraphQLObjectType({
       type: TaskType,
       args: { id: {type: GraphQLID} },
       resolve(parent, args) {
-        return lodash.find(tasks, {id: args.id});
+        return lodash.find(Task, {id: args.id});
       }
     },
     project: {
       type: ProjectType,
       args: { id: {type: GraphQLID} },
       resolve(parent, args) {
-        return lodash.find(projects, {id: args.id});
+        return lodash.find(Project, {id: args.id});
       }
     },
     tasks: {
       type: new GraphQLList(TaskType),
       resolve(parent, args) {
-        return tasks;
+        return Task;
       }
     },
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project;
       }
     }
   })
