f4300ffd22ca90fc78bb071eff98944ed602cedd
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-16 16:29:26 -0500

    6: uuuggggg finally >,'/ had to update nodejs for it to work and the whole MongoDB thing took forever to start with, kinda ridiculous, this task should be worth more than 6 points, that was such a pain

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
+mongoose.connect('mongodb+srv://krisCrossApplesauce:3kVkCI432s7GmXA0@mongodbforgraphqlprojec.pvql7ai.mongodb.net/?retryWrites=true&w=majority&appName=MongoDBForGraphQLProject');
+
 const app = express();
+
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
