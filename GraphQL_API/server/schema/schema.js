2705d161ede3b8e4a6098326370758a50df1578b
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-16 14:16:54 -0500

    4: I don't know if graphql4_1 and graphql4_2 are correct, but I also don't know how to use them so I guess it doesn't really matter? Also, nodemon app results in an error, but it doesn't say it has to work so I'm hoping it's fine :T

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,9 +1,9 @@
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLList, GraphQLSchema } = require('graphql');
 const lodash = require('lodash');
 
 const tasks = [
-  { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)' },
-  { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order' }
+  { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)', projectId: '1' },
+  { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order', projectId: '1' }
 ];
 
 const projects = [
@@ -17,7 +17,14 @@ const TaskType = new GraphQLObjectType({
     id: {type: GraphQLID},
     title: {type: GraphQLString},
     weight: {type: GraphQLInt},
-    description: {type: GraphQLString}
+    description: {type: GraphQLString},
+    project: {
+      type: TaskType,
+      args: { projectId: {type: GraphQLID} },
+      resolve(parent, args) {
+        return lodash.find(projects, {id: args.projectId});
+      }
+    }
   }
 });
 
@@ -27,7 +34,14 @@ const ProjectType = new GraphQLObjectType({
     id: {type: GraphQLID},
     title: {type: GraphQLString},
     weight: {type: GraphQLInt},
-    description: {type: GraphQLString}
+    description: {type: GraphQLString},
+    tasks: {
+      type: TaskType,
+      args: { type: GraphQLList },
+      resolve(parent, args) {
+        return lodash.filter(tasks, {projectId: parent.id});
+      }
+    }
   }
 });
 
